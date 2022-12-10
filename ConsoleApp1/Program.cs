a55965a4dae7932986c7b1c6a30609130c117eaa
iiina <iiina@DESKTOP-A4R70HK>
2022-12-10 20:20:55 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    abstract class Figure
+    {
+        public abstract void Circle();
+      
+        public abstract void Rectangle();
+    }
+    class Area : Figure
+    {
+        public override void Rectangle()
+        {
+            Console.WriteLine("사각형 그리기");
+        }
+        public override void Circle()
+        {
+            Console.WriteLine("원그리기");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area area = new Area();
+            area.Circle();
+            area.Rectangle();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public interface IFigure
+{
+   void Rectangle();
+    void Circle();
+
+}
+class figure : IFigure
+{
+    public void Rectangle()
+    {
+        Console.WriteLine("사각형 그리기");
+    }
+    public void Circle()
+    {
+        Console.WriteLine("원그리기");
+    }
+}
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            figure a=new figure();
+            a.Rectangle();
+            a.Circle();
+        }
+    }
+}
